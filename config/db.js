abde42ea0df31f6f5cb66e51ee2d923525931788
Akshay <rathodak315@gmail.com>
2025-03-11 18:57:21 +0530

    fixing deployment

--- config/db.js
+++ config/db.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    console.log('process.env.MONGO_URI', process.env.MONGO_URI);
+// const connectDB = async () => {
+//     console.log('process.env.MONGO_URI', process.env.MONGO_URI);
 
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI);
+//         console.log('MongoDB connected successfully');
+//     } catch (err) {
+//         console.error('MongoDB connection failed:', err);
+//         process.exit(1);
+//     }
+// };
+
+const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected successfully');
-    } catch (err) {
-        console.error('MongoDB connection failed:', err);
-        process.exit(1);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.error('MongoDB Connection Error:', error);
+        process.exit(1); // Exit on failure
     }
 };
 
